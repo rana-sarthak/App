a53eafe018c4c06dd5c04e0445544514655c1497
Sarthak Rana <pro.go.istabul.day2@gmail.com>
2024-06-01 18:12:50 +0530

    Code setup

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public string Message {get;set;}
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Message = "Kya haal hai";
+    }
+}
